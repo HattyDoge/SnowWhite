660e631db30fff20cf0456e5de50f4845e446745
HattyDoge <126104442+HattyDoge@users.noreply.github.com>
2023-12-07 11:01:25 +0100

    Added Client program

--- ConsoleAppSocket/ConsoleAppClient/Client.cs
+++ ConsoleAppSocket/ConsoleAppClient/Client.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Remoting.Channels;
 
 namespace ConsoleAppClient
 {
@@ -10,6 +13,46 @@ namespace ConsoleAppClient
     {
         static void Main(string[] args)
         {
+            string data = null;
+            //Array of bytes
+            byte[] bytes = new byte[1024];
+
+            Console.WriteLine("\nProgramma Client\n");
+
+            //Establish the remote endpoint for the socket
+            //Dns.GetHostName returns the name of the host running the application
+
+            IPAddress ipAddress = IPAddress.Parse("10.1.0.8");
+
+            IPEndPoint remoteEndPoint = new IPEndPoint(ipAddress, 11000);
+            //Create a TCP/IP socket
+            Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            //Connect the remote endpoint and send
+            try
+            {
+                sender.Connect(remoteEndPoint);
+                //Istruction for debug
+                Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
+                //Encode the data string into a byte array
+                byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+                //Send the data through the socket.
+                int bytesSent = sender.Send(msg);
+
+                do
+                {
+                    int bytesRec = sender.Receive(msg);
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                } while (data.IndexOf("<EOF>") <= -1);
+
+                Console.WriteLine("Echoed test = {0}", data);
+
+                //Release the socket.
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
         }
     }
 }
--- ConsoleAppSocket/ConsoleAppSocket/Server.cs
+++ ConsoleAppSocket/ConsoleAppSocket/Server.cs
@@ -11,19 +11,20 @@ namespace ConsoleAppServer
         static void Main(string[] args)
         {
             string data = null;
-
+            //Array of bytes
             byte[] bytes = new byte[1024];
 
             Console.WriteLine("\nProgramma Server\n");
             //Establish the local endpoint for the socket
             //Dns.GetHostName returns the name of the host running the application
-        #if true
+        #if false
             IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
             IPAddress ipAddress = ipHostInfo.AddressList[1];
         #elif false
-            IPAddress ipAddress = IPAddress.Any;//Fornisce un indirizzo IP che indica che il server deve attendere l'attività dei client su tutte le interfacce di rete. Questo campo è di sola lettura
+            IPAddress ipAddress = IPAddress.Any;//Fornisce un indirizzo IP che indica che il server deve attendere 
+                                                //l'attività dei client su tutte le interfacce di rete. Questo campo è di sola lettura
         #else
-            IPAddress ipAddress = IPAddress.Parse("192.168.0.8");
+            IPAddress ipAddress = IPAddress.Parse("10.1.0.8");
         #endif
             IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
             //Create a TCP/IP socket
@@ -31,20 +32,47 @@ namespace ConsoleAppServer
             //Bind the socket to the local endpoint and listen for incoming connections.
             try
             {
+                //Associate the IP address and the port
                 listener.Bind(localEndPoint);
-                listener.Listen(10);
+                //Number of maximum client
+                listener.Listen(1);
 
                 //Start listening for connections
                 while(true)
                 {
                     Console.WriteLine("Waiting for connection...");
                     //Program is suspended while waiting for an incoming connection.
+                    Socket handler = listener.Accept();
+                    data = null;
+
+                    //An incoming connection needs to be processed
+                    do
+                    {
+                        //Receive data from client
+                        int bytesRec = handler.Receive(bytes);
+                        //Transforms data
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        //Receives until final instruction marked with <EOF>
+                    } while (data.IndexOf("<EOF>") <= -1);
+
+                    //Show the data on the console.
+                    Console.WriteLine("Text received : {0}", data);
+
+                    #region Echo the data back to the client.
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    handler.Send(msg);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                    #endregion
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
         }
     }
 }
